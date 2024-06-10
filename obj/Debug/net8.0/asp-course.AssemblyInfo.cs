f65823b436197cd5f69293aad9562a6a11b86e71
Nyasper <herreragonzalo39@gmail.com>
2024-06-09 23:23:28 -0300

    fix

--- obj/Debug/net8.0/asp-course.AssemblyInfo.cs
+++ obj/Debug/net8.0/asp-course.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("asp-course")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cac67cbbce4ff8d29ccbcebc7d7a67159739870")]
 [assembly: System.Reflection.AssemblyProductAttribute("asp-course")]
 [assembly: System.Reflection.AssemblyTitleAttribute("asp-course")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
